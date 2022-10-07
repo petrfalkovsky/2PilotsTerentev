ce9525d841dc1a899a4b416b12ce6813be835700
Петр Фальковский <petrfalkovsky@yandex.ru>
2022-10-07 16:41:01 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Digdez1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Выберите уровень:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(255, 25);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(51, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(397, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(232, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Попробовать открыть сейф";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::_2PilotsTerentev.Resource1.background;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Drawing;
+using System.Reflection.Emit;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Digdez1
+{
+    public partial class Form1 : Form
+    {
+        int n;
+        string s = "Внимание!";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            label1.Font = new Font("Times New Roman", 14, FontStyle.Bold);
+            label1.ForeColor = System.Drawing.Color.Transparent;
+            comboBox1.Items.Add("3");
+            comboBox1.Items.Add("4");
+            comboBox1.Items.Add("5");
+            comboBox1.Items.Add("6");
+        }
+
+        private PictureBox[,] pb = null;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (pb != null)
+                foreach (var pictureBox in pb)
+                    Controls.Remove(pictureBox);
+
+            if (int.TryParse(comboBox1.Text, out n))
+            {
+                n = Convert.ToInt32(comboBox1.Text);
+            }
+
+            if (n > 6 || n <= 2)
+            {
+                MessageBox.Show("Выберите 3, 4, 5 или 6 уровнь сложности.", s, MessageBoxButtons.OK);
+            }
+            else
+            {
+                pb = new PictureBox[n, n];
+                for (int i = 0; i < n; i++)
+                    for (int j = 0; j < n; j++)
+                    {
+                        pb[i, j] = new PictureBox();
+                        pb[i, j].Location = new System.Drawing.Point(20 + i * 100, 50 + j * 100);
+                        pb[i, j].Size = new System.Drawing.Size(63, 63);
+                        pb[i, j].TabIndex = i;
+                        pb[i, j].Load("../../Resources/sprite3.png");
+                        pb[i, j].Tag = 0;
+                        Controls.Add(pb[i, j]);
+                        int row = i;
+                        int column = j;
+                        pb[i, j].Click += (x, y) => { pbClick(row, column); };
+                    }
+                Rotation();
+            }
+        }
+       
+
+        private void Rotation()
+        {
+            var rnd = new Random();
+            for (int i = 0; i < 50; i++)
+                pbClick(rnd.Next(n), rnd.Next(n), true);
+        }
+
+        private bool pbClick(int row, int column, bool init = false)
+        {
+            bool win = !init;
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == row || j == column)
+                    {
+                        Image image = pb[i, j].Image;
+                        image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                        pb[i, j].Image = image;
+
+                        pb[i, j].Tag = ((int)pb[i, j].Tag + 1) % 2;
+                    }
+                    win &= (int)pb[i, j].Tag == (int)pb[0, 0].Tag;
+                }
+
+            if (win)
+                MessageBox.Show("Ура! У вас получилось!!!", s, MessageBoxButtons.OK);
+            return win;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Silver;
+            this.Left = 350;
+            this.Top = 40;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Точно закрываем?\nМы пока не научились сохранять ваши достижения =(", s, MessageBoxButtons.YesNoCancel);
+            if (dr == DialogResult.Yes)
+                e.Cancel = false;
+            else
+                e.Cancel = true;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+      
+    }
+}
